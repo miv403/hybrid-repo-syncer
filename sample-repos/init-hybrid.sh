a0526c795f278aab3e63c4d74740d21dfe5f8202
miv403 <miv403@duck.com>
2026-07-24 14:50:14 +0300

    init-hybrid.sh: removed remote adding

--- sample-repos/init-hybrid.sh
+++ sample-repos/init-hybrid.sh
@@ -3,21 +3,21 @@
 mkdir hybrid
 [ $? -eq 1 ] && exit 1
 
-if [ -z "$1" ]; then
-  N=1
-else
-  N=$1
-fi
+# if [ -z "$1" ]; then
+#   N=1
+# else
+#   N=$1
+# fi
 
 cd hybrid && git init
 git config receive.denyCurrentBranch updateInstead
 touch .gitignore && git add . && git commit -m "hybrid::initial commit"
 
-if [ ! -d "../repo-$N" ];
-then
-  echo "repo-$N doesn't exists"
-  exit 1
-fi
+# if [ ! -d "../repo-$N" ];
+# then
+#   echo "repo-$N doesn't exists"
+#   exit 1
+# fi
 
-git remote add "repo-$N-origin" "../repo-$N.git"
-git remote add "repo-$N-clone" "../repo-$N"
+# git remote add "repo-$N-origin" "../repo-$N.git"
+# git remote add "repo-$N-clone" "../repo-$N"
